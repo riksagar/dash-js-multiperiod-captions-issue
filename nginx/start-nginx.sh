4b38b6bd558412077c0b82047285c9584247f423
Rik Sagar <com.remove.github@sagar.remove.org>
2025-06-17 11:52:38 -0700

    initial commit

---
+++ nginx/start-nginx.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+nginx -c '/Users/rsagar/Docs/ProjectData/Dynamic Playback Stitching/multiperiod-captions-test/nginx/nginx.conf'
+
+
+echo HINT:
+echo  - nginx -s stop
+echo  - nginx -s restart
